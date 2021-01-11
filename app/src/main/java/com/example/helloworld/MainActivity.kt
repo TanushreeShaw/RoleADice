04f5933285049767acffe8c4f465ded1a987d142
TanushreeShaw <shawtanushree17@gmail.com>
2021-01-11 21:47:51 +0530

    "Role Two Dices"

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+    lateinit var diceImage2 : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        diceImage=findViewById(R.id.dice_image)
+        diceImage2=findViewById(R.id.dice_image2)
+
+    }
+
+    /**
+     * Click listener for the Roll button.
+     */
+    private fun rollDice() {
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+
+    }
+
+    private fun getRandomDiceImage() : Int {
+
+        val randomInt=(1..6).random()
+
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+
+            else -> R.drawable.dice_6
+        }
+
+    }
+}
